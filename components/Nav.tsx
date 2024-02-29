eb0a9209bb6f33f6d3b42eeaa8882903e1f4b81c
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-02-29 10:38:08 +0700

    Add create post btn

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 import React from "react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
     <nav className="w-full flex-between mb-16 pt-4">
-      <Link href="/" className="flex">
+      <Link href="/" className="flex flex-center gap-2">
         <Image
           src="/assets/images/logo.svg"
           alt="logo"
@@ -13,7 +14,30 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
+        <p className="logo_text">Promptopia</p>
       </Link>
+
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex flex_center gap-1 md:gap-3">
+            <Link href="/" className="black_btn">
+              Create Post
+            </Link>
+            <button className="outline_btn">Sign out</button>
+            <Link href="/">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="logo"
+                width={30}
+                height={30}
+                className="object-contain"
+              />
+            </Link>
+          </div>
+        ) : (
+          <p>nani</p>
+        )}
+      </div>
     </nav>
   );
 };
