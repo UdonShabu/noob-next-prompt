d74833b6cc8687fb63c856c1b034474534c33dff
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-02-29 09:54:35 +0700

    1-QZ Global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "@styles/global.css"; //Can u fix the error?
 
 export const metadata: Metadata = {
   title: "Promptopia",
