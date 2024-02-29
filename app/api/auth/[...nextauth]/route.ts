ae2dd927ec0a7251b709532034c6daf34b6b3381
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-02-29 17:14:33 +0700

    Fix option and add env

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,35 +10,36 @@ const handler = NextAuth({
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
-
-  async session({ session }: any) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }: any) {
-    try {
-      await connectToDB();
-
-      const userExists = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({ session }: any) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }: any) {
+      try {
+        await connectToDB();
 
-      if (!userExists) {
-        await User.create({
+        const userExists = await User.findOne({
           email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
         });
-      }
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
